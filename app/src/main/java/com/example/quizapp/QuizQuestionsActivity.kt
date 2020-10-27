0b4a67b46a02c0cfb9ad6b286be43d54cac64e82
Fatih Can <ybsfatihcan@gmail.com>
2020-10-27 14:34:22 +0300

    image and progressbar - option updated

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,19 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionList = Constants.getQuestions()
         Log.i("Questions Size", "${questionList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionList[currentPosition -1]
+
+        progressBar.progress = currentPosition
+        textview_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        textview_question.text = question!!.question
+        imageview_image.setImageResource(question.image)
+
+        textview_option_one.text = question.optionOne
+        textview_option_two.text = question.optionTwo
+        textview_option_three.text = question.optionThree
+        textview_option_four.text = question.optionFour
     }
 }
