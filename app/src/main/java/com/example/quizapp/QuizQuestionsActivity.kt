19aaa2d03f905e0358859daf18ce07314e16b93a
Fatih Can <ybsfatihcan@gmail.com>
2020-10-28 15:37:09 +0300

    Result - score - name update

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
@@ -17,10 +18,13 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mQuestionList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
     private var mCorrectAnswers: Int = 0
+    private var mUserName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
 
         mQuestionList = Constants.getQuestions()
 
@@ -96,8 +100,12 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                         mCurrentPosition <= mQuestionList!!.size ->{
                             setQuestion()
                         }else ->{
-                        Toast.makeText(this, "You have successfully completed the Quiz", Toast.LENGTH_SHORT).show()
-                    }
+                        val intent = Intent(this, ResultActivity::class.java)
+                        intent.putExtra(Constants.USER_NAME, mUserName)
+                        intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+                        intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionList!!.size)
+                        startActivity(intent)
+                      }
                    }
                 }else{
                     val question = mQuestionList?.get(mCurrentPosition -1)
--- app/src/main/java/com/example/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -1,11 +1,24 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val username = intent.getStringExtra(Constants.USER_NAME)
+        textview_name.text = username
+        val totalQuestion = intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
+        val correctAnswer = intent.getIntExtra(Constants.CORRECT_ANSWERS,0)
+
+        textview_score.text = "Your Score is $correctAnswer out of $totalQuestion"
+
+        button_finish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
